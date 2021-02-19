e3300660641550a8919f55339b47b1ee17fd8ab1
devian5 <acgarage@outlook.es>
2021-02-19 19:56:38 +0100

    I have created the app file script to connect express

--- api/app.js
+++ api/app.js
@@ -1,1 +1,11 @@
-const express = require('express')
+const express = require('express').Router();
+const app = express()
+
+const port = 3000
+
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log('I am ready!!')
+})
+
